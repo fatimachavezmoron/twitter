9b412d47838d814998b2a3bf35ab983e31b5b138
Fatima Chavez <fatimachavezmoron@gmail.com>
2023-09-12 17:09:52 +0200

    add icons to posts

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -3,6 +3,7 @@ import useLoginModal from '@/hooks/useLoginModal';
 import { formatDistanceToNowStrict } from 'date-fns';
 import { useRouter } from 'next/router';
 import React, { useCallback, useMemo } from 'react'
+import { AiOutlineHeart, AiOutlineMessage } from 'react-icons/ai';
 import Avatar from '../Avatar';
 
 interface PostItemProps {
@@ -65,8 +66,38 @@ const PostItem: React.FC<PostItemProps> = ({data, userId}) => {
               {createdAt}
             </span>
           </div>
-          <div>
+          <div className='text-white mt-1'>
             {data.body}
+          </div>
+          <div className='flex flex-row items-center mt-3 gap-10'>
+            <div className='flex 
+            flex-row 
+            items-center
+           text-neutral-500 
+            gap-2 
+            cursor-pointer 
+            transition 
+            hover:text-sky-500
+            '>
+              <AiOutlineMessage size={20}/>
+              <p>
+                {data.comments?.length || 0}
+              </p>
+            </div>
+            <div className='flex 
+            flex-row 
+            items-center
+           text-neutral-500 
+            gap-2 
+            cursor-pointer 
+            transition 
+            hover:text-sky-500
+            '>
+              <AiOutlineHeart size={20}/>
+              <p>
+                {data.comments?.length || 0}
+              </p>
+            </div>
           </div>
         </div>
       </div>
