e6c621a19b58ccb82dcc80716a4644910801c63a
Fatima Chavez <fatimachavezmoron@gmail.com>
2023-09-14 13:54:43 +0200

    add like on postItem

--- pages/api/like.ts
+++ pages/api/like.ts
@@ -1,7 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next";
 
-import { NextApiRequest, NextApiResponse } from 'next';
-import  serverAuth  from '@/libs/serverAuth';
-import prisma from '@/libs/prismadb'
+import prisma from '@/libs/prismadb';
+import serverAuth from "@/libs/serverAuth";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST' && req.method !== 'DELETE') {
@@ -30,12 +30,41 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     let updatedLikedIds = [...(post.likedIds || [])];
 
     if (req.method === 'POST') {
-      updatedLikedIds.push(currentUser.id)
+      updatedLikedIds.push(currentUser.id);
+      
+      // NOTIFICATION PART START
+      try {
+        const post = await prisma.post.findUnique({
+          where: {
+            id: postId,
+          }
+        });
+    
+        if (post?.userId) {
+          await prisma.notification.create({
+            data: {
+              body: 'Someone liked your tweet!',
+              userId: post.userId
+            }
+          });
+    
+          await prisma.user.update({
+            where: {
+              id: post.userId
+            },
+            data: {
+              hasNotification: true
+            }
+          });
+        }
+      } catch(error) {
+        console.log(error);
+      }
+      // NOTIFICATION PART END
     }
 
     if (req.method === 'DELETE') {
-      updatedLikedIds = updatedLikedIds
-      .filter((likedId) => likedId !== currentUser.id)
+      updatedLikedIds = updatedLikedIds.filter((likedId) => likedId !== currentUser?.id);
     }
 
     const updatedPost = await prisma.post.update({
@@ -48,8 +77,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     });
 
     return res.status(200).json(updatedPost);
-
-  } catch(error) {
+  } catch (error) {
     console.log(error);
     return res.status(400).end();
   }
