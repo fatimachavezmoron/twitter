511c4d80ee2d4041b1608340616202ecf2405961
Fatima Chavez <fatimachavezmoron@gmail.com>
2023-09-14 18:21:46 +0200

    add comments on real time to other comments

--- components/Form.tsx
+++ components/Form.tsx
@@ -8,6 +8,7 @@ import { toast } from 'react-hot-toast';
 import axios from 'axios'
 import Button from './Button';
 import Avatar from './Avatar';
+import usePost from '@/hooks/usePost';
 
 
 interface formProps {
@@ -26,6 +27,7 @@ const Form: React.FC<formProps> = ({
 
   const {data: currentUser} = useCurrentUser();
   const {mutate: mutatePosts} = usePosts();
+  const {mutate: mutatePost} = usePost(postId as string)
 
   const [body, setBody] = useState('');
   const [isLoading, setIsLoading] = useState(false);
@@ -34,19 +36,24 @@ const Form: React.FC<formProps> = ({
     try {
       setIsLoading(true);
 
-      await axios.post('/api/posts', {body});
+      const url = isComment 
+      ? `/api/comments?postId=${postId}`
+      : '/api/posts';
+
+      await axios.post(url, {body});
 
       toast.success('Tweet created');
 
       setBody('');
       mutatePosts();
+      mutatePost()
 
     } catch(error) {
       toast.error('Something went wrong')
     } finally {
       setIsLoading(false)
     }
-  }, [body, mutatePosts])
+  }, [body, mutatePosts, isComment, postId])
 
   return (
     <div className='border-b-[1px] border-neutral-800 px-5 py-2'>
